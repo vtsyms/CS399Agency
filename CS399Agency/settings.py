07e81e038b54f5e2a3d3000c37bef3454fc5e938
Veronika Alves <vt77@nau.edu>
2015-02-16 14:56:05 -0700

    Route to Index established
    One change in settings, added index and base.html files, and added path
    in urls.

--- CS399Agency/settings.py
+++ CS399Agency/settings.py
@@ -83,5 +83,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'CS399Agency/static'),)
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'CS399Agency/templates'),)
