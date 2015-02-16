f2bd6a3ef0e8f7cb0358d1e460f517d703632fad
Veronika Alves <vt77@nau.edu>
2015-02-16 15:08:18 -0700

    Added about page

--- CS399Agency/urls.py
+++ CS399Agency/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from views import index
+from views import index, about
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', index)
+    url(r'^$', index),
+    url(r'^about', about)
 )
