112d58706095c88161182d3ab44d2a3dc4a84158
Veronika Alves <vt77@nau.edu>
2015-02-17 11:22:39 -0700

    Model tables added to admin page

--- CS399Agency/admin.py
+++ CS399Agency/admin.py
@@ -1,2 +1,10 @@
 from django.contrib import admin
+from models import Contact
+from models import Contest
+from models import Refer
+from models import Signup
 
+admin.site.register(Contact)
+admin.site.register(Contest)
+admin.site.register(Refer)
+admin.site.register(Signup)
