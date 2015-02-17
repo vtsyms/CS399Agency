02c16805c2e14774361c050e587fa5dbb4fd3be2
sk367 <sirbubbles42@gmail.com>
2015-02-17 00:33:16 -0700

    removed old model files

--- CS399Agency/models/contact.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Contact(models.Model):
-    email = models.EmailField(max_length=254)
-    firstName = models.CharField(max_length=128, default="")
-    lastName = models.CharField(max_length=128, default="")
-    created = models.DateTimeField(auto_now_add=True)
-    subject = firstName = models.CharField(max_length=128, default="")
-    message = models.TextField(default="")
--- CS399Agency/models/signup.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Signup(models.Model):
-    firstName = models.CharField(max_length=128, default="")
-    lastName = models.CharField(max_length=128, default="")
-    email = models.EmailField(max_length=254)
-    created = models.DateTimeField(auto_now_add=True)
